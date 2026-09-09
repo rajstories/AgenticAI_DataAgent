96689ce9c66c5c6d72225b435476aba19f0193f7
Lxdia <rajshrivastav283815@gmail.com>
2026-09-09 10:09:42 +0530

    Refactor demo entry point

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from agents.data_agent import data_agent
 from langchain_core.messages import HumanMessage
 
+
+def run_demo():
+    demo_request = {
+        "messages": [
+            HumanMessage(content="I want to extract the data from the API endpoint 'https://pokeapi.co/api/v2/pokemon' and save it to data/extract folder in the csv folder")
+        ],
+        "route_response": ""
+    }
+
+    return data_agent.invoke(demo_request)
+
+
 if __name__ == "__main__":
-    response = data_agent.invoke(
-        {"messages":[HumanMessage(content="I want to extract the data from the API endpoint 'https://pokeapi.co/api/v2/pokemon' and save it to data/extract folder in the csv folder")],
-         "route_response": ""}
-    )
+    response = run_demo()
 
     print(response)
